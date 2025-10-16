412d840f941ddcae85ad5ba66f97e7f9d3bf7cfe
Stefan Nieuwenhuis <stefannhs@gmail.com>
2025-10-16 20:14:42 +0200

    fix integration tests

--- fastapi/tests/test_avg_pr_interval.py
+++ fastapi/tests/test_avg_pr_interval.py
@@ -29,18 +29,3 @@ def test_get_avg_pr_interval(client, test_session) -> None:
     assert data["repo_name"] == MOCK_REPO_NAME
     assert data["avg_interval_seconds"] == SECONDS_PER_DAY
     assert data["pr_count"] == MOCK_PR_COUNT
-
-def test_get_agg_event_counts(client, test_session) -> None:
-    """Test FastAPI endpoint should fetch Aggregated Event Counts for a given offset correctly"""
-
-    MOCK_OFFSET_MINUTES = 10
-
-
-    test_session.execute("""
-            INSERT INTO event_counts_by_type (type, bucket_day, time_bucket, count)
-                VALUES (%s, %s, %s, %s, %s)
-        """, ("PushEvent", "2025-10-16", "2025-10-16 16:55:00.000000+0000", 27))
-
-    response = client.get(f"/v1/agg_event_counts?offset_minutes={MOCK_OFFSET_MINUTES}")
-    assert response.status_code == 200
-
